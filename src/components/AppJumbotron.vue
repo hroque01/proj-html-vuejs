ee1d4a05c0729ca84abedbd222d9dbb4c99eba97
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-21 20:06:45 +0800

    Add Jumbotron to App.vue and adding template

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: "App",
+}
+</script>
+
+<template>
+    <section class="jumbotron">
+        <div class="container">
+            <div class="JumboText">
+                <h1>
+                    Indeperident professional who works remotely to help with your business.
+                </h1>
+                <p>
+                    Mauris aliquam libero, fermentum quis eros, massa sagittis.
+                    Lectus mattis elit purus venenatis.
+                </p>
+                <button class="button">
+                    What is a VA excatly?
+                </button>
+            </div>
+            <div>
+                <img src="/info-5-400x444.jpg" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+
+.jumbotron {
+    color: $font-white;
+    background-color: $color-bg;
+
+    .container {
+        display: flex;
+    }
+}
+</style>
