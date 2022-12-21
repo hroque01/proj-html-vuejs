4291a69a50b214371cd22b5b5d7a06187e39058d
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-21 19:28:39 +0800

    Header css

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,16 @@ export default {
 </script>
 
 <template>
-  <AppHeaderVue />
+  <header>
+    <AppHeaderVue />
+  </header>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables.scss' as *;
+
+header {
+  background-color: $color-bg;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -40,25 +40,76 @@ export default {
 
     <div class="container">
         <div class="header-nav">
-            <a href="#">
-                <img src="/logo.png" alt=""> <!-- this is the img logo-->
-            </a>
-        </div>
-
-        <nav>
-            <ul>
-                <li v-for="(link, index) in links" :key="index">
-                    <a :href="links.url">
-                        {{ link.text }}
+            <div class="nav-left">
+                <!-- Logo -->
+                <div class="logo">
+                    <a href="#">
+                        <img src="/logo.png" alt=""> <!-- this is the img logo-->
                     </a>
-                </li>
-            </ul>
-        </nav>
+                </div>
 
+                <!-- Navigation -->
+                <nav>
+                    <ul>
+                        <li class="navigation" v-for="(link, index) in links" :key="index">
+                            <a :href="link.url">
+                                {{ link.text }}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- Consulting -->
+            <div class="consultation">
+                <div class="call-center">
+                    <font-awesome-icon icon="fa-solid fa-headset" />
+                    <span> 1.800.123.4567 </span>
+                </div>
+                <!-- Button -->
+                <button class="button btn_white">
+                    Get a free consultation
+                </button>
+            </div>
+        </div>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables.scss' as *;
+
+.header-nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.875rem;
+    color: $font-white;
+
+    .nav-left {
+
+        display: flex;
+        align-items: center;
+
+        ul {
+            display: flex;
+            gap: 1.25rem;
+        }
+
+        .navigation {
+
+            a {
+                color: $font-white;
+            }
+
+        }
+    }
+
+    .consultation {
+        display: flex;
+        gap: 1.25rem;
+    }
+
+
+}
 </style>
