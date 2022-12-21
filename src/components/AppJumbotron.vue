9d957f429eeacbf238b79a452a32f220a9ebe741
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-21 20:19:17 +0800

    Create template Jumbotron without css

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -7,6 +7,7 @@ export default {
 <template>
     <section class="jumbotron">
         <div class="container">
+            <!-- Jumbotron text -->
             <div class="JumboText">
                 <h1>
                     Indeperident professional who works remotely to help with your business.
@@ -19,9 +20,28 @@ export default {
                     What is a VA excatly?
                 </button>
             </div>
+
+            <!-- Image -->
             <div>
                 <img src="/info-5-400x444.jpg" alt="">
             </div>
+
+            <!-- Buttons Folder and $39 on right -->
+            <div class="little-buttons-right">
+                <div class="buttons-icon">
+                    <a href="#">
+                        <font-awesome-icon icon="fa-solid fa-folder" />
+                        <span>DEMOS</span>
+                    </a>
+                </div>
+                <div class="buttons-icon">
+                    <a href="#">
+                        <font-awesome-icon icon="fa-solid fa-folder" />
+                        <span>DEMOS</span>
+                    </a>
+                </div>
+            </div>
+
         </div>
     </section>
 </template>
@@ -36,6 +56,18 @@ export default {
 
     .container {
         display: flex;
+    }
+
+    .buttons-icon {
+        width: 80px;
+        height: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: white;
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHeadset } from '@fortawesome/free-solid-svg-icons'
+import { faHeadset, faFolder } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
 
-library.add(faHeadset,)
+library.add(faHeadset, faFolder,)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
